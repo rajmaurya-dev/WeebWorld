6996f25ac893d4d746dee59ea53be5df7f05bdce
Prashant Raj <blcc.raj@gmail.com>
2023-05-10 16:25:44 +0530

    added signout

--- components/NavigationCard.js
+++ components/NavigationCard.js
@@ -4,6 +4,7 @@ import { BiHome, BiGroup, BiLogOut } from "react-icons/bi";
 import { BsFillBookmarkHeartFill } from "react-icons/bs";
 import { RiNotificationBadgeLine } from "react-icons/ri";
 import Link from "next/link";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
 export default function NavigationCard() {
   const router = useRouter();
   const { asPath: pathname } = router;
@@ -16,7 +17,10 @@ export default function NavigationCard() {
     "text-sm md:text-md flex gap-1 md:gap-3 py-3 my-1 bg-socialBlue text-white md:-mx-7 px-6 md:px-7 rounded-md shadow-md shadow-gray-300 items-center";
   const nonActiveLinksClasses =
     "text-sm md:text-md flex gap-1 md:gap-3 py-2 my-2 hover:bg-blue-500 hover:bg-opacity-20 md:-mx-4 px-6 md:px-4 rounded-md transition-all hover:scale-110 hover:shadow-md shadow-gray-300 items-center";
-
+  const supabase = useSupabaseClient();
+  const logout = async () => {
+    await supabase.auth.signOut();
+  };
   return (
     <Card noPadding={true}>
       <div className="md:px-12 py-2 flex md:flex-col gap-2 justify-between shadow-md shadow-gray-500 md:shadow-none">
@@ -63,10 +67,10 @@ export default function NavigationCard() {
           <RiNotificationBadgeLine />
           <span className="hidden md:block">Notifications</span>
         </Link>
-        <Link href="/login" className={nonActiveLinksClasses}>
+        <button onClick={logout} className={nonActiveLinksClasses}>
           <BiLogOut />
           <span className="hidden md:block">Logout</span>
-        </Link>
+        </button>
       </div>
     </Card>
   );
